661fe0353272af402ddaaf523529554846d2eaa5
Kabir Batra <kabirbatrakkbb@gmail.com>
2021-01-08 00:33:38 +0530

    first commit

---
+++ ignition.js
@@ -0,0 +1,70 @@
+const canvas = document.getElementById('ignition');
+const ctx = canvas.getContext('2d');
+
+let numOfpartlces = 600;
+let particles = [];
+
+setInterval(() => {
+  x = undefined; //canvas.width = undefined and set to false/true using button for start/stop
+  y = undefined;
+}, 200);
+
+class Particle {
+  constructor(x, y, size, weight) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    // this.color = color;
+    this.weight = weight;
+  }
+
+  draw = () => {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fillStyle = 'crimson';
+    ctx.fill();
+  };
+
+  update = () => {
+    // this.size -= 0.005;
+    // if (this.size < 0) {
+    //   // this.x = mouse.x + Math.random() * 20 - 10;
+    //   // this.y = mouse.y + Math.random() * 20 - 10;
+    //   this.size = Math.random() * 5 + 2;
+    //   this.weight = Math.random() * 20 - 0.5;
+    // }
+    this.size = Math.random() * 5 + 2;
+    this.weight = Math.random() * 15 - 0.5;
+    this.y += this.weight;
+    // this.weight += 0.00001; //speed
+
+    if (this.y > canvas.height - this.size) {
+      // this.weight *= -1;
+      this.y = 0;
+    }
+  };
+}
+
+init = () => {
+  particles = [];
+  for (let i = 0; i < numOfpartlces; i++) {
+    let x = Math.floor((Math.random() * canvas.width) / 8 + 358); // ignition width
+    let y = 0;
+    let size = Math.random() * 5 + 2;
+    let weight = 1; //speed
+    // let color = '#fff';
+    particles.push(new Particle(x, y, size, weight));
+  }
+};
+
+animate = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < numOfpartlces; i++) {
+    particles[i].update();
+    particles[i].draw();
+  }
+  requestAnimationFrame(animate);
+};
+
+init();
+animate();
---
+++ stars.js
@@ -0,0 +1,65 @@
+const canvas3 = document.getElementById('stars');
+const ctx3 = canvas3.getContext('2d');
+canvas3.height = window.innerHeight;
+canvas3.width = window.innerWidth;
+const numOfstars = 500;
+let stars = [];
+
+class Star {
+  constructor(x3, y3, size3, weight3, boolean) {
+    this.x3 = x3;
+    this.y3 = y3;
+    this.size3 = size3;
+    this.weight3 = weight3;
+    this.boolean = boolean;
+  }
+
+  draw3 = () => {
+    ctx3.beginPath();
+    ctx3.arc(this.x3, this.y3, this.size3, 0, Math.PI * 2);
+    ctx3.fillStyle = '#fff';
+    ctx3.fill();
+  };
+
+  update3 = () => {
+    if (this.boolean % 17 == 0) {
+      this.size3 = Math.random() * 2 + 4; //////////////////size3 change ///twinkle
+    } else if (this.boolean % 7 == 0) {
+      this.size3 = Math.random() * 2 + 3;
+    } else if (this.boolean % 3 == 0) {
+      this.size3 = Math.random() * 2 + 2;
+    } else {
+      this.size3 = Math.random() * 1 + 1; //////////////////size3 change ///twinkle
+    }
+    this.y3 += this.weight3;
+    // this.weight3 += 0.01; //////acceleration
+    if (this.y3 > canvas3.height - this.size3) {
+      // this.weight3 *= -1; /////bounce
+      this.y3 = 0;
+    }
+  };
+}
+
+init3 = () => {
+  stars = [];
+  for (let i = 0; i < numOfstars; i++) {
+    let x3 = Math.random() * canvas3.width;
+    let y3 = Math.random() * canvas3.height;
+    let size3 = Math.random() * 10 + 2;
+    let weight3 = 1; //////weight3 is speeddddddd
+    let boolean = i;
+    stars.push(new Star(x3, y3, size3, weight3, boolean));
+  }
+};
+
+animate3 = () => {
+  ctx3.clearRect(0, 0, canvas3.width, canvas3.height);
+  for (let i = 0; i < numOfstars; i++) {
+    stars[i].update3();
+    stars[i].draw3();
+  }
+  requestAnimationFrame(animate3);
+};
+
+init3();
+animate3();
